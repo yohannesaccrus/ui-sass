0a130ee926fd38950ad850bc4c6eeaa37b99b504
yohannesaccrus <yohanesaccrus014@gmail.com>
2019-11-14 12:29:20 +0700

    added responsive

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,25 +5,32 @@ const App = () => {
     <div className="app">
       <div className="app__overlay" />
       <div className="app__text">
-        <h1 className="app__title">"You can do it"</h1>
+        <h1 className="app__title">
+          Let's have <span>Crush</span> on <span>Code</span>
+        </h1>
         <p className="app__subtitle">
           a simple template to brighten and start your adventure in UI/UX world
           using React and SASS
         </p>
         <div className="app__socials">
-          <a href="https://www.instagram.com/yohanesaccrus/" className="app__social">
+          <a
+            href="https://www.instagram.com/crushoncode/"
+            className="app__social"
+          >
             <i className="fab fa-instagram" />
-            <p>@yohanesaccrus</p>
+            <p>@crushoncode</p>
           </a>
-          <a href="https://web.facebook.com/yohannes.accroozdelcacciaderto" className="app__social">
-            <i className="fab fa-facebook-square" />
-            <p>Accrus</p>
-          </a>
-          <a href="https://www.youtube.com/watch?v=fLE9pChmC7M" className="app__social">
+          <a
+            href="https://www.youtube.com/channel/UCmEkB9GWMPJ6H833W-gKMAw"
+            className="app__social"
+          >
             <i className="fab fa-youtube" />
-            <p>yohanes accrus</p>
+            <p>CrushOnCode</p>
           </a>
-         
+          <a href="https://github.com/yohannesaccrus" className="app__social">
+            <i className="fab fa-github" />
+            <p>yohannesaccrus</p>
+          </a>
         </div>
       </div>
     </div>
